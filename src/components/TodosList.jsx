9505853c78ed428b494722d58f3a16d6aca7fcbf
Dishant <dishantbaghel55@gmail.com>
2023-11-03 13:13:26 +0530

    optimize the code and use useeffect to render the buttons

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,108 +1,85 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { data } from '../staticData/buttonData';
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenToSquare,
+  faTrash,
+  faCheck,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { data } from "../staticData/buttonData";
 
-const TodosList = ({ todos, setTodos, editTodo, setEditTodo, isEdit, setIsEdit, errorMessage, setErrorMessage }) => {
-  const [editedText, setEditedText] = useState('');
-  const [filter, setFilter] = useState('All'); // this is for filter the todos
-  const [active, setActive] = useState(null)   // this is for change the color of the buttons on click
-
-  const handleFilter = (id,name) => {
-    setFilter(name)
-    setActive(id)
-  };
-
-  let filteredTodos = todos;
-
-  if (filter === 'Completed') {
-    filteredTodos = todos.filter(todo => todo.completed);
-  } else if (filter === 'Incomplete') {
-    filteredTodos = todos.filter(todo => !todo.completed);
-  }
-
-  const handleComplete = (id) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
-  };
-
-  const onChangeInput = (e) => {
-    setEditedText(e.target.value);
-    setErrorMessage('');
-  };
-
-  const handleEdit = (id) => {
-    setIsEdit(true);
-    setEditTodo(id);
-    const editedTodo = todos.find((todo) => todo.id === id);
-    setEditedText(editedTodo.title);
-  };
-
-  const handleSave = () => {
-    const trimmedText = editedText.trim();
-    if (trimmedText !== '') {
-      const updatedTodos = todos.map((todo) =>
-        todo.id === editTodo ? { ...todo, title: trimmedText } : todo
-      );
-      setTodos(updatedTodos);
-    } else {
-      setErrorMessage('Todo cannot be empty');
-    }
-    setEditTodo(null);
-    setIsEdit(false);
-  };
-
-  const handleDelete = ({ id }) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
+const TodosList = ({
+  todos,
+  setTodos,
+  editTodo,
+  setEditTodo,
+  isEdit,
+  setIsEdit,
+  errorMessage,
+  setErrorMessage,
+  active,
+  filteredTodos,
+  handleComplete,
+  handleDelete,
+  handleEdit,
+  handleFilter,
+  handleSave,
+  editedText,
+  onChangeInput
+}) => {
   return (
     <div>
-      <div className='status'>
-      {
-        data.map((ele)=>{
-          return(
-            <button className={ele.id===active?"active":"status-btn"} onClick={()=>handleFilter(ele.id,ele.name)} key={ele.id}>{ele.name}</button>
-          )
-        })
-      }
+      <div className="status">
+        {data.map((ele) => {
+          return (
+            <button
+              className={ele.id === active ? "active" : "status-btn"}
+              onClick={() => handleFilter(ele.id, ele.name)}
+              key={ele.id}
+            >
+              {ele.name}
+            </button>
+          );
+        })}
       </div>
       {filteredTodos.map((todo) => (
-        <div className='display-todos' key={todo.id}>
-          <li className='list'>
-            <div className='single-todo'>
-            <div className='checkbox-text'>
-            <input
-                className='check-box'
-                type='checkbox'
-                checked={todo.completed}
-                onChange={() => handleComplete(todo.id)}
-              />
-              {isEdit && editTodo === todo.id ? (
+        <div className="display-todos" key={todo.id}>
+          <li className="list">
+            <div className="single-todo">
+              <div className="checkbox-text">
                 <input
-                  type='text'
-                  className='todos-input-field'
-                  value={editedText}
-                  onChange={onChangeInput}
+                  className="check-box"
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => handleComplete(todo.id)}
                 />
+                {isEdit && editTodo === todo.id ? (
+                  <input
+                    type="text"
+                    className="todos-input-field"
+                    value={editedText}
+                    onChange={onChangeInput}
+                  />
+                ) : (
+                  todo.title
+                )}
+              </div>
+              {todo.completed == true ? (
+                <div className="done-msg">Done</div>
               ) : (
-                todo.title
-              )}</div>
-              {todo.completed == true ? <div className='done-msg'>Done</div> :""}
-              
+                ""
+              )}
             </div>
             {isEdit && editTodo === todo.id ? (
-              <button className='btn' onClick={handleSave}>
+              <button className="btn" onClick={handleSave}>
                 <FontAwesomeIcon icon={faCheck} />
               </button>
             ) : (
               <>
-                <button className='btn' onClick={() => handleEdit(todo.id)}>
+                <button className="btn" onClick={() => handleEdit(todo.id)}>
                   <FontAwesomeIcon icon={faPenToSquare} />
                 </button>
-                <button className='btn' onClick={() => handleDelete(todo)}>
+                <button className="btn" onClick={() => handleDelete(todo)}>
                   <FontAwesomeIcon icon={faTrash} />
                 </button>
               </>
